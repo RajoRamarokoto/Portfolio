1b0a843519990792a90b70854141c93dfd2ab787
Ramarokoto Rajo Ali Velo <alivelo@goulamina-jv.com>
2025-02-26 15:11:57 +0000

    Updated contact

--- script.js
+++ script.js
@@ -71,3 +71,33 @@ $(".list-toggle").click(function() {
     $(this).closest("ul").toggleClass("collapsed").toggleClass("expanded");
   
   });
+
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    var form = event.target;
+    var formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            form.reset(); // Reset the form
+            document.getElementById('form-message').style.display = 'block'; // Show the success message
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert("Oops! There was a problem submitting your form");
+                }
+            });
+        }
+    }).catch(error => {
+        alert("Oops! There was a problem submitting your form");
+    });
+});
